626fe8baa1ced06b16667869e55310a53c3984f4
Ivan <ivanmoll07@gmail.com>
2021-03-02 04:22:42 +0100

    Add unauthorized method to response a requests unauthorized

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function unauthorizedUser(): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => 'Unauthorized User',
+        ], 403);
+    }
 }
