5facd9a719cf8e0c1595f6694c287c3252e585e2
Ivan <ivanmoll07@gmail.com>
2021-03-08 01:13:20 +0100

    Change app url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/TFG_UOC/homeweb-backend/public'),
 
     'asset_url' => env('ASSET_URL', null),
 
