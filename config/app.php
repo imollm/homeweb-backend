2c3b3c37aa56c3a27e81da770d5394e6ec713fce
Ivan <ivanmoll07@gmail.com>
2021-03-08 12:28:45 +0100

    Change some wrong names

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/TFG_UOC/homeweb-backend/public'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
