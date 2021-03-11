024dbd9dff1c826dca1793441cdf7ff0e4b19cd9
Ivan <ivanmoll07@gmail.com>
2021-03-11 02:59:30 +0100

    Added create method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,9 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class CategoryController
+ * @package App\Http\Controllers
+ */
 class CategoryController extends Controller
 {
+    public function create(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+        ]);
+
+        $categoryName = strtolower($request->input('name'));
+
+        $categoryExists = Category::where('name', '=', $categoryName)->first();
+
+        if ($categoryExists) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category exists',
+            ]);
+        }
+
+        if (Auth::user()->can('create', Category::class)) {
+
+            $category = new Category();
+            $category->name = $categoryName;
+
+            if ($category->save()) {
+                return response()->json([
+                    'success' => true,
+                    'data' => $category,
+                    'message' => 'Category was added correctly',
+                    'exists' => $categoryExists
+                ]);
+            }
+        } else {
+            return $this->unauthorizedUser();
+        }
+
+    }
+
+    /**
+     * Return all categories
+     *
+     * @return JsonResponse
+     */
     public function all(): JsonResponse
     {
         $categories = Category::all();
