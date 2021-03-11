d007035fef0fffe1b1277e41055df28451384d45
Ivan <ivanmoll07@gmail.com>
2021-03-11 02:58:10 +0100

    Change the way to pass params of call function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(PropertySeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            RoleSeeder::class,
+            CategorySeeder::class,
+            UserSeeder::class,
+            PropertySeeder::class
+        ]);
     }
 }
