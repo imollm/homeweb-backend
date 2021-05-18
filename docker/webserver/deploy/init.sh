c5272724c9b677af43c4e90d1a0e9377672e690f
Ivan <ivanmoll07@gmail.com>
2021-05-18 16:17:27 +0200

    Dockerized backend

---
+++ docker/webserver/deploy/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT_DIR=/var/www/html/homeweb-backend
+
+cd "$PROJECT_DIR" && echo "Project directory ${PROJECT_DIR}"
+
+if [ ! -d "${PROJECT_DIR}/vendor" ]; then
+    echo "Installing dependencies" &&
+    composer install
+fi
+
+cp .env.example .env && echo "Publish .env vars" && php artisan config:clear
+
+echo "Migrating, seeding, generate keys and passport install" &&
+php artisan migrate:fresh --seed &&
+php artisan key:generate &&
+php artisan passport:install
+
+exit 0
