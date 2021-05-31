f73cf5aaca488afd622623982a8451026e10dca7
Ivan <ivanmoll07@gmail.com>
2021-05-31 23:16:28 +0200

    Add sed command to add gmail account password on .env file. Is not recommended push passwords on github repo.

--- docker/webserver/deploy/init.sh
+++ docker/webserver/deploy/init.sh
@@ -9,7 +9,9 @@ if [ ! -d "${PROJECT_DIR}/vendor" ]; then
     composer install
 fi
 
-cp .env.example .env && echo "Publish .env vars" && php artisan config:clear
+cp .env.example .env && echo "Publish .env vars"
+sed -i 's/MAIL_PASSWORD=/MAIL_PASSWORD=q4m12ZTAIeTf/g' .env && echo "Config email password"
+php artisan config:clear && echo "Config clear"
 
 echo "Migrating, seeding, generate keys and passport install" &&
 php artisan migrate:fresh --seed &&
